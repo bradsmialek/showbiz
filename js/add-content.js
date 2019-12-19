c0bc274e76a7a623f2f3ec29522cfd81d3bcacbc
spacecowboi <cunningham2192@gmail.com>
2019-12-19 10:01:03 -0800

    you had it right inititally dude

---
+++ js/add-content.js
@@ -0,0 +1,10 @@
+'use strict';
+var bike = prompt('Would you like to learn more?');
+
+if (bike == "yes") {
+  alert('Good!');
+}else if(bike == "no") {
+  alert('No problem!');
+}else {
+  confirm('Consider our about page');
+}
